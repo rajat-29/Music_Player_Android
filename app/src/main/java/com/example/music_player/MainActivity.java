f2ed78a13a22acca6939f325427c6f7f2e23bc01
rajat-29 <rajat295gupta@gmail.com>
2019-06-30 11:02:40 +0530

    songs from storage

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.music_player;
 
 import android.Manifest;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.karumi.dexter.Dexter;
@@ -12,9 +14,13 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView myListViewForSongs;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void runtimePermission()
     {
+        // Using Dexter to get Permissions
         Dexter.withActivity(this)
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        display();
 
                     }
 
@@ -49,4 +58,50 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).check();
     }
+
+    // Function to find songs
+    public ArrayList<File> findSong(File file)
+    {
+        ArrayList<File>arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        for(File singleFile: files)
+        {
+            if(singleFile.isDirectory() && !singleFile.isHidden())
+            {
+                arrayList.addAll(findSong(singleFile));
+            }
+            else
+            {
+                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav"))
+                {
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+
+        return arrayList;
+    }
+
+    //Function to display song list
+    void display()
+    {
+        // Calling function to get all Songs and store in arraylist mysongs
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+
+        //loop to get all file name
+        for(int i=0;i<mySongs.size();i++)
+        {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        myListViewForSongs.setAdapter(myAdapter);
+
+    }
+
+
 }
