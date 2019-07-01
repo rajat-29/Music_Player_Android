4a1c25db95102499c77c7bffcdbdfdb74d8ae08b
rajat-29 <rajat295gupta@gmail.com>
2019-07-01 16:16:13 +0530

    java code 1

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.music_player;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -100,6 +103,19 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
         myListViewForSongs.setAdapter(myAdapter);
+
+        myListViewForSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String songName = myListViewForSongs.getItemAtPosition(position).toString();
+
+                startActivity(new Intent(getApplicationContext(),PlayerActivity.class)
+                .putExtra("songs",mySongs).putExtra("songname",songName));
+
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/music_player/PlayerActivity.java
+++ app/src/main/java/com/example/music_player/PlayerActivity.java
@@ -1,16 +1,28 @@
 package com.example.music_player;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class PlayerActivity extends AppCompatActivity {
 
     Button btn_next,btn_prev,btn_pause;
     TextView songTextLabel;
     SeekBar songSeekbar;
+
+    static MediaPlayer myMediaPlayer;
+    int position;
+    String sname;
+
+    ArrayList<File> mySongs;
+    Thread updateseekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +36,48 @@ public class PlayerActivity extends AppCompatActivity {
         songTextLabel = (TextView)findViewById(R.id.songLabel);
 
         songSeekbar = (SeekBar)findViewById(R.id.seekBar);
+
+        updateseekBar = new Thread()
+        {
+            @Override
+            public void run() {
+
+                int totalDuration = myMediaPlayer.getDuration();
+                int currentPosition = 0;
+
+                while(currentPosition<totalDuration)
+                {
+                    try
+                    {
+                        sleep(500);
+                        currentPosition = myMediaPlayer.getCurrentPosition();
+                        songSeekbar.setProgress(currentPosition);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+
+        if(myMediaPlayer!=null)
+        {
+            myMediaPlayer.stop();
+            myMediaPlayer.release();
+        }
+
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+
+        mySongs = (ArrayList) bundle.getParcelableArrayList("songs");
+
+        sname = mySongs.get(position).getName().toString();
+
+        String songName = i.getStringExtra("songname");
+
+        songTextLabel.setText(songName);
+        songTextLabel.setSelected(true);
     }
 }
