660796fee2f7aea6e4a682a1b1bd806f3542619a
rajat-29 <rajat295gupta@gmail.com>
2019-07-01 14:02:47 +0530

    interface 1

--- app/src/main/java/com/example/music_player/PlayerActivity.java
+++ app/src/main/java/com/example/music_player/PlayerActivity.java
@@ -2,12 +2,27 @@ package com.example.music_player;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    Button btn_next,btn_prev,btn_pause;
+    TextView songTextLabel;
+    SeekBar songSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        btn_next = (Button)findViewById(R.id.next);
+        btn_prev = (Button)findViewById(R.id.prev);
+        btn_pause = (Button)findViewById(R.id.pause);
+
+        songTextLabel = (TextView)findViewById(R.id.songLabel);
+
+        songSeekbar = (SeekBar)findViewById(R.id.seekBar);
     }
 }
